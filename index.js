a382051fac23ec503765d9daeed88d715f6e8f72
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-15 20:32:31 +1000

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array) {
+  const newArray = []
+  for (let i of array) {
+    newArray.push(`Thank you, ${i}, for the wonderful surprise gift!`)
+  }
+  return newArray
+}
+
+function countDown(number) {
+  while (number >=0) {
+    console.log(number)
+    number--
+  }
+}
